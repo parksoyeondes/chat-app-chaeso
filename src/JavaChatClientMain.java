8014cca01fcd45727fc36c9cbecae4be14f35e33
Park Soyeon <710soyeon@naver.com>
2025-11-05 15:19:59 +0900

    하이를 추가했슨

--- src/JavaChatClientMain.java
+++ src/JavaChatClientMain.java
@@ -3,7 +3,7 @@
 
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener; // 하이
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
